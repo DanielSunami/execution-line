967211fde10ba64b6dc6be4e16d7161bae421dc5
Daniel Sunami <sunami_daniel@hotmail.com>
2020-07-06 23:43:30 -0300

    added browser support & added execution end fn

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-'use stric'
+"use strict";
 
-function ExecutionLine(ctx, customObject) {
-	ctx = ctx || {};
-	customObject = customObject || null;
+(function() {
+	function ExecutionLine(ctx, customObject) {
+		ctx = ctx || {};
+		customObject = customObject || null;
 
-	let line = [];
+		let line = [], _self = this;
 
-	function hasNext() { if(line.length > 0) return true; else return false; }
+		function hasNext() { if(line.length > 0) return true; else return false; }
 
-	function nextFn() {
-		if(hasNext()) return line.shift();
-		else return function(){};
+		function nextFn() {
+			if(hasNext()) return line.shift();
+			else return _self.onexecutionend;
+		}
+
+		function next() {
+			if(customObject === null)
+				nextFn().call(ctx, next);
+			else 
+				nextFn().call(ctx, customObject, next);
+		}
+
+		this.onexecutionend = function() {}
+
+		this.hook = function(fn) {
+			if(!fn || !(fn instanceof Array || fn instanceof Function))
+				throw new TypeError('You must hook a Function or an Array of Functions');
+
+			if(fn instanceof Array) {
+				for(let i = 0; i < fn.length; i++) {
+					if(!(fn[i] instanceof Function)) throw new TypeError('You must hook a Array of Functions');
+				}
+
+				line.push(...fn);
+			} else line.push(fn);
+		}
+
+		this.exec = function() {
+			if(!hasNext()) return false;
+			next();
+		}
+
+		this.clear = function() {
+			line = [];
+		}
+
 	}
 
-	function next() {
-		if(customObject === null)
-			nextFn().call(ctx, next);
-		else 
-			nextFn().call(ctx, customObject, next);
-	}
-
-	this.hook = function(fn) {
-		if(!fn || !(fn instanceof Array || fn instanceof Function))
-			throw new TypeError('You must hook a Function or an Array of Functions');
-
-		if(fn instanceof Array) {
-			for(let i = 0; i < fn.length; i++) {
-				if(!(fn[i] instanceof Function)) throw new TypeError('You must hook a Array of Functions');
-			}
-
-			line.push(...fn);
-		} else line.push(fn);
-	}
-
-	this.exec = function() {
-		if(!hasNext()) return false;
-		next();
-	}
-
-	this.clear = function() {
-		line = [];
-	}
-}
-
-module.exports = ExecutionLine;
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+		module.exports = ExecutionLine;
+	else
+		window.ExecutionLine = ExecutionLine;
+})();
